b74a072cb1cb3dc63fa52bed68f6ce96fe503041
Mateusz <bmatteo@interia.pl>
2024-01-15 16:45:41 +0100

    init

---
+++ src/main/java/pl/comarch/camp/it/book/store/controllers/CommonController.java
@@ -0,0 +1,28 @@
+package pl.comarch.camp.it.book.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.comarch.camp.it.book.store.dao.IBookDAO;
+
+@Controller
+public class CommonController {
+
+    private final IBookDAO bookDAO;
+
+    public CommonController(IBookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @RequestMapping(path = {"/main", "/", "/index"}, method = RequestMethod.GET)
+    public String main(Model model) {
+        model.addAttribute("books", this.bookDAO.getAll());
+        return "index";
+    }
+
+    @RequestMapping(path = "/contact", method = RequestMethod.GET)
+    public String contact() {
+        return "contact";
+    }
+}
